70d792db19a39431e5cbfda80237a7ef7aea94d6
Fesposito9197 <kekkoesp8@gmail.com>
2022-10-28 10:32:22 +0200

    fix

--- snack-6/js/main.js
+++ snack-6/js/main.js
@@ -2,7 +2,7 @@ const number = [ 5 , 6, 7 ,8 , 9 , 10, 11, 12, 13]
 
 
 let somma = 0
-for (let i = 0; i < number.lengtht; i++) {
+for (let i = 0; i < number.length; i++) {
     if (i % 2 !== 0) {
         somma += number[i]
         
