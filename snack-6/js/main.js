f46d134c7b8c4c3ed04fc4274250e1c41ba2a098
Fesposito9197 <kekkoesp8@gmail.com>
2022-10-28 10:31:39 +0200

    snack fatto

---
+++ snack-6/js/main.js
@@ -0,0 +1,12 @@
+const number = [ 5 , 6, 7 ,8 , 9 , 10, 11, 12, 13]
+
+
+let somma = 0
+for (let i = 0; i < number.lengtht; i++) {
+    if (i % 2 !== 0) {
+        somma += number[i]
+        
+    }
+    
+}
+console.log(somma)
